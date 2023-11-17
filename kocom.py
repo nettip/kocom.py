acb98c1c831b8629add3dc7d3717a571ec1f3ec8
nettip <73051068+nettip@users.noreply.github.com>
2023-11-17 17:40:31 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -30,15 +30,15 @@ BUF_SIZE = 100
 read_write_gap = 0.03  # minimal time interval between last read to write
 polling_interval = 300  # polling interval
 
-header_h = 'aa55'
+header_h = 'EF'
 trailer_h = '0d0d'
-packet_size = 21  # total 21bytes
-chksum_position = 18  # 18th byte
+packet_size = 10  # total 21bytes
+chksum_position = 9  # 18th byte
 
 type_t_dic = {'30b':'send', '30d':'ack'}
 seq_t_dic = {'c':1, 'd':2, 'e':3, 'f':4}
 # device_t_dic = {'01':'wallpad', '0e':'light', '2c':'gas', '36':'thermo', '3b': 'plug', '44':'elevator', '48':'fan'}  # 2023.08 AC, AIR 추가
-device_t_dic = {'01': 'wallpad', '0e': 'light', '2c': 'gas', '36': 'thermo', '39': 'ac', '3b': 'plug', '44': 'elevator', '48': 'fan', '98': 'air'}
+device_t_dic = {'01': 'wallpad', '0e': 'light', '2c': 'gas', '78': 'thermo', '39': 'ac', '7A': 'plug', 'A0': 'elevator', '48': 'fan', '98': 'air'}
 cmd_t_dic = {'00':'state', '01':'on', '02':'off', '3a':'query'}
 room_t_dic = {'00':'livingroom', '01':'room1', '02':'room2', '03':'room3', '04':'kitchen'}
 
@@ -46,7 +46,7 @@ type_h_dic = {v: k for k, v in type_t_dic.items()}
 seq_h_dic = {v: k for k, v in seq_t_dic.items()}
 device_h_dic = {v: k for k, v in device_t_dic.items()}
 cmd_h_dic = {v: k for k, v in cmd_t_dic.items()}
-room_h_dic = {'livingroom':'00', 'myhome':'00', 'room1':'01', 'room2':'02', 'room3':'03', 'kitchen':'04'}
+room_h_dic = {'livingroom':'01', 'myhome':'00', 'room1':'02', 'room2':'03', 'room3':'04', 'kitchen':'01'}
 
 # mqtt functions ----------------------------
 
